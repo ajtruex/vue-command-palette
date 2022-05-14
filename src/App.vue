35e4431abeb764571379921a9553e7dec48d6de1
Andrew Truex <andrewtruex@gmail.com>
2022-05-14 05:46:53 -0400

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import CommandPalette from "./components/CommandPalette.vue"
+
+const displayCommandPalette = ref(false)
+</script>
+
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" @click="displayCommandPalette = !displayCommandPalette" />
+  <CommandPalette v-if="displayCommandPalette" />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/CommandPalette.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="command-palette">
+    <h1>My Command Palette</h1>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style>
+</style>
