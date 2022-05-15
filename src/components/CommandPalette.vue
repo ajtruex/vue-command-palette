a7df2b869b0fb7255a550f850e6596dae29d4629
Andrew Truex <andrewtruex@gmail.com>
2022-05-15 04:20:26 -0400

    gave up at 28:05 of the video after writing the v-model

--- src/components/CommandPalette.vue
+++ src/components/CommandPalette.vue
@@ -1,11 +1,29 @@
+<script setup>
+import { ref } from "vue"
+
+const commandFilterText = ref("")
+const commandList = ref([
+  { id: 123, title: "Command Title", alias: [], hotkeys: [] },
+  { id: 456, title: "Command Title", alias: [], hotkeys: [] },
+  { id: 789, title: "Command Title", alias: [], hotkeys: [] }
+
+])
+</script>
+
 <template>
   <div class="command-palette">
     <h1>My Command Palette</h1>
+    <input type="text" v-model="commandFilterText" />
+    <ul>
+      <li v-for="command in commandList" :key="command.id">
+        {{ command.title }}
+        {{ command.hotkeys }}
+      </li>
+    </ul>
   </div>
 </template>
 
-<script setup>
-</script>
+
 
 <style>
 </style>
